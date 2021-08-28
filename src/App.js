19748d0f9d7d8c834c4b94917e20e660b1c96547
boze <bozeha@gmail.com>
2021-08-28 11:02:31 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ListOfGames from "./components/listOfGames";
+
+function App() {
+  return (
+    <div className="App">
+      <ListOfGames />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/listOfGames.tsx
@@ -0,0 +1,10 @@
+
+import React,{Component} from "react";
+
+class ListOfGames extends Component {
+    render(){
+        return <h1>xxxxx</h1>
+    }
+}
+
+export default ListOfGames;
